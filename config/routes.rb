722421b79842c71e9da26ff11469901799aeec18
benhare <benahare@gmail.com>
2015-08-03 22:33:54 +1000

    added navigation and routed pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'static_pages#contact'
 
   resources :orders, only: [:index, :show, :new, :create]
   # Example of regular route:
