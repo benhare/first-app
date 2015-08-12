db956069fb82839a07651fd1ffe8b9b265382a76
benhare <benahare@gmail.com>
2015-08-12 20:34:59 +1000

    added mailer method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,11 +11,9 @@ class StaticPagesController < ApplicationController
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(:from => @email, 
-	  	:to => 'benahare@gmail.com', 
-	  	:subject => "A new contact form message from #{@name}", 
-	  	:body => @message).deliver
+  UserMailer.contact_form(@email, @name, @message).deliver
   end
+
 end
 
 
